10973ec7f1fc2a4d61a8f2a3fc1b4c71cb8154c9
Mate Lakat <mlakat@mlakat.hu>
2013-01-24 00:37:47 +0000

    First successfull remastered iso run - Cleanup needed

--- scripts/create-xs-disk.sh
+++ scripts/create-xs-disk.sh
@@ -3,6 +3,8 @@
 # Useful: 
 # http://blogs.citrix.com/2010/10/18/how-to-install-citrix-xenserver-from-a-usb-key-usb-built-from-windows-os/
 # http://scnr.net/blog/index.php/archives/177
+# http://www.ithastobecool.com/tag/xenclient/
+# http://forums.citrix.com/message.jspa?messageID=1479624
 
 set -eux
 
@@ -31,11 +33,11 @@ sudo mkdir -p /tmp/xstgt && sudo mount -t vfat /dev/mapper/loop0p1 /tmp/xstgt
 
 sudo cp -a /tmp/xsbp/* /tmp/xstgt/
 
-sudo cp /tmp/xstgt/boot/isolinux/* /tmp/xstgt/
+#sudo cp /tmp/xstgt/boot/isolinux/* /tmp/xstgt/
 
-sudo mv /tmp/xstgt/{iso,sys}linux.cfg
-sudo mv /tmp/xstgt/{iso,sys}linux.bin
-sudo cp /usr/lib/syslinux/mboot.c32 /tmp/xstgt/
+#sudo mv /tmp/xstgt/{iso,sys}linux.cfg
+#sudo mv /tmp/xstgt/{iso,sys}linux.bin
+#sudo cp /usr/lib/syslinux/mboot.c32 /tmp/xstgt/
 
 
 # Initrd re-generation
@@ -61,18 +63,24 @@ sudo find . -print | sudo cpio -o -H newc | xz --format=lzma | sudo dd of=/tmp/x
 cd ..
 
 # bash --rcfile /dev/null -i
-sudo cp syslinux.cfg /tmp/xstgt/syslinux.cfg
+sudo cp syslinux.cfg /tmp/xstgt/boot/isolinux/isolinux.cfg
 
 sudo umount /tmp/xsbp
+
+echo '/boot 1000' > /tmp/sortlist
+sudo mkisofs -joliet -joliet-long -r -b boot/isolinux/isolinux.bin \
+-c boot/isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
+-boot-info-table -sort /tmp/sortlist -V "My Custom XenServer ISO" -o customxs.iso /tmp/xstgt/
+
 sudo umount /tmp/xstgt
 
 sudo kpartx -d xsdrive.raw
 
 # Convert and attach the raw image to the machine
 vboxmanage storagectl STUFF --name "IDECONTROLLER" --remove || true
-vboxmanage closemedium disk xsdrive.vdi --delete
+#vboxmanage closemedium disk xsdrive.vdi --delete
 
-vboxmanage convertfromraw xsdrive.raw xsdrive.vdi --format VDI
+#vboxmanage convertfromraw xsdrive.raw xsdrive.vdi --format VDI
 
 vboxmanage storagectl STUFF --name "IDECONTROLLER" --add ide
-vboxmanage storageattach "STUFF" --storagectl IDECONTROLLER --type hdd --medium "`pwd`/xsdrive.vdi" --port 0 --device 0
+vboxmanage storageattach "STUFF" --storagectl IDECONTROLLER --type dvddrive --medium "`pwd`/customxs.iso" --port 0 --device 0
