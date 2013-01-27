92613d294e3636b9f758a9c31a83d6fdaa6d299e
Mate Lakat <mlakat@mlakat.hu>
2013-01-27 12:53:48 +0000

    Extract create_vm

---
+++ scripts/create_vm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eux
+
+vboxmanage createvm --name "STUFF" --register --basefolder "`pwd`/STUFF"
+vboxmanage createhd --filename "`pwd`/stuff.vdi" --size=20000
+
+vboxmanage storagectl STUFF --name "SCSICONTROLLER" --add scsi
+vboxmanage storageattach "STUFF" --storagectl SCSICONTROLLER --type hdd \
+--medium "`pwd`/stuff.vdi" --port 0 --device 0
+
+vboxmanage modifyvm STUFF --ioapic on
+vboxmanage modifyvm STUFF --memory 2048
+
+vboxmanage modifyvm STUFF --nic1 nat --cableconnected1 on 
+vboxmanage modifyvm STUFF --natpf1 SSH,tcp,,2222,,22
+
+#vboxsdl --startvm STUFF
+# Do the install, shut down
+
+#vboxheadless -s STUFF &
