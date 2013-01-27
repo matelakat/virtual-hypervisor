a7351cf3c15440b96043277a644fb82396901676
Mate Lakat <mlakat@mlakat.hu>
2013-01-27 18:37:23 +0000

    Increase memory to 4 gigabytes

--- scripts/create_vm.sh
+++ scripts/create_vm.sh
@@ -9,7 +9,7 @@ vboxmanage storageattach "STUFF" --storagectl SCSICONTROLLER --type hdd \
 --medium "`pwd`/stuff.vdi" --port 0 --device 0
 
 vboxmanage modifyvm STUFF --ioapic on
-vboxmanage modifyvm STUFF --memory 2048
+vboxmanage modifyvm STUFF --memory 4096
 
 vboxmanage modifyvm STUFF --nic1 nat --cableconnected1 on 
 vboxmanage modifyvm STUFF --natpf1 SSH,tcp,,2222,,22
