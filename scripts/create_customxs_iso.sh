9d3591511fb95857720e2dd325fb61298c8b57b7
Mate Lakat <mlakat@mlakat.hu>
2013-02-07 06:50:12 +0000

    Use temporary directories

--- scripts/create_customxs_iso.sh
+++ scripts/create_customxs_iso.sh
@@ -7,45 +7,39 @@
 # http://forums.citrix.com/message.jspa?messageID=1479624
 # http://blogs.citrix.com/2012/07/13/xs-unattended-install/
 
-set -eux
+set -eu
 
 [ -e hypervisor.iso ]
 
+WORKINGDIR=`pwd`
+ISOROOT=`mktemp -d`
+
 # extract iso
-rm -rf xstgt && mkdir -p xstgt && 7z x hypervisor.iso -oxstgt
+7z x "$WORKINGDIR/hypervisor.iso" "-o${ISOROOT}"
 
-# Initrd re-generation
-rm -rf ./initrd && mkdir ./initrd
 
-# extract initrd
-cd ./initrd
+INITRDROOT=`mktemp -d`
 
-(
-cat << FAKEROOT
-zcat ../xstgt/install.img | cpio -idum
+cat << FAKEROOT | fakeroot bash
+cd "$INITRDROOT"
+zcat "$ISOROOT/install.img" | cpio -idum
 
 # Do the remastering
-cp ../data/answerfile.xml ./
-cp ../data/postinst.sh ./
-cp ../data/firstboot.sh ./
+cp "$WORKINGDIR/data/answerfile.xml" ./
+cp "$WORKINGDIR/data/postinst.sh" ./
+cp "$WORKINGDIR/data/firstboot.sh" ./
 
 # Re-pack initrd
-find . -print | cpio -o -H newc | xz --format=lzma | dd of=../xstgt/install.img
-
+find . -print | cpio -o -H newc | xz --format=lzma | dd of="${ISOROOT}/install.img"
 FAKEROOT
-) | fakeroot bash
 
-cd ..
+rm -rf "$INITRDROOT"
 
-rm -rf ./initrd
-
-# bash --rcfile /dev/null -i
-cp ./data/isolinux.cfg ./xstgt/boot/isolinux/isolinux.cfg
+cp "${WORKINGDIR}/data/isolinux.cfg" "${ISOROOT}/boot/isolinux/isolinux.cfg"
 
 echo '/boot 1000' > sortlist
 mkisofs -joliet -joliet-long -r -b boot/isolinux/isolinux.bin \
 -c boot/isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
--boot-info-table -sort sortlist -V "My Custom XenServer ISO" -o customxs.iso ./xstgt/
+-boot-info-table -sort sortlist -V "My Custom XenServer ISO" -o customxs.iso "$ISOROOT"
 
-rm -rf ./xstgt
-
+rm -rf "$ISOROOT"
