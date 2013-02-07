0c61cabb4d7cdb63b4af3ab30e82d47f6e937f95
Mate Lakat <mlakat@mlakat.hu>
2013-02-07 08:09:18 +0000

    Quiet switches for cpio, human -like log messages

--- scripts/create_customxs_iso.sh
+++ scripts/create_customxs_iso.sh
@@ -30,14 +30,15 @@ TOP_DIR=$(cd $(dirname "$0") && cd .. && pwd)
 ISOROOT=`mktemp -d`
 
 # extract iso
-7z x "$ORIGINAL_ISO" "-o${ISOROOT}"
-
+echo "Extracting $ORIGINAL_ISO to $ISOROOT"
+7z x "$ORIGINAL_ISO" "-o${ISOROOT}" > /dev/null
 
 INITRDROOT=`mktemp -d`
 
+echo "Remastering $ISOROOT/install.img"
 cat << FAKEROOT | fakeroot bash
 cd "$INITRDROOT"
-zcat "$ISOROOT/install.img" | cpio -idum
+zcat "$ISOROOT/install.img" | cpio -idum --quiet
 
 # Do the remastering
 cp "$TOP_DIR/data/answerfile.xml" ./
@@ -45,16 +46,19 @@ cp "$TOP_DIR/data/postinst.sh" ./
 cp "$TOP_DIR/data/firstboot.sh" ./
 
 # Re-pack initrd
-find . -print | cpio -o -H newc | xz --format=lzma | dd of="${ISOROOT}/install.img"
+find . -print | cpio -o --quiet -H newc | xz --format=lzma > "${ISOROOT}/install.img"
 FAKEROOT
 
+echo "Removing $INITRDROOT"
 rm -rf "$INITRDROOT"
 
 cp "$TOP_DIR/data/isolinux.cfg" "${ISOROOT}/boot/isolinux/isolinux.cfg"
 
+echo "Create new iso: customxs.iso"
 echo '/boot 1000' > sortlist
-mkisofs -joliet -joliet-long -r -b boot/isolinux/isolinux.bin \
+mkisofs -quiet -joliet -joliet-long -r -b boot/isolinux/isolinux.bin \
 -c boot/isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
 -boot-info-table -sort sortlist -V "My Custom XenServer ISO" -o customxs.iso "$ISOROOT"
 
+echo "Removing $ISOROOT"
 rm -rf "$ISOROOT"
