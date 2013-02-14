81b61dc67c5663c674b3a161dba28687917031b7
Mate Lakat <mlakat@mlakat.hu>
2013-02-14 23:46:34 +0000

    less verbose logging, status added to display name

--- scripts/_xs_vm_starter.sh
+++ scripts/_xs_vm_starter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+set -eu
 
 function print_usage_and_quit
 {
@@ -27,7 +27,7 @@ SR=$(xe sr-create name-label=CUSTOMXSISO type=iso device-config:location="$REPOD
 PBD=$(xe pbd-list sr-uuid=$SR --minimal)
 
 # VM install
-VXS=$(xe vm-install template="Other install media" new-name-label="$MACHINE_NAME")
+VXS=$(xe vm-install template="Other install media" new-name-label="$MACHINE_NAME - created (Step 1 of 3)")
 NETWORK=$(xe network-list name-label="$NETWORK_NAME" --minimal)
 MEM="6GiB"
 
@@ -46,6 +46,7 @@ xe vm-cd-add cd-name=customxs.iso uuid=$VXS device=1
 # First-time start
 xe vm-param-set uuid=$VXS actions-after-reboot=Destroy
 xe vm-start uuid=$VXS
+xe vm-param-set uuid=$VXS name-label="$MACHINE_NAME - booted from iso (Step 2 of 3)"
 
 # Wait for shut
 while ! xe vm-param-get param-name=power-state uuid=$VXS | grep halted; do sleep 1; done
@@ -55,6 +56,7 @@ xe vm-param-set uuid=$VXS actions-after-reboot=Restart
 
 # Start again, so install completes
 xe vm-start uuid=$VXS
+xe vm-param-set uuid=$VXS name-label="$MACHINE_NAME - first boot (Step 3 of 3)"
 
 # Get rid of ISO SR
 xe pbd-unplug uuid=$PBD
--- scripts/xs_start_create_vm_with_cdrom.sh
+++ scripts/xs_start_create_vm_with_cdrom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+set -eu
 
 function print_usage_and_quit
 {
