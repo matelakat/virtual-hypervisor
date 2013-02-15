be878684e0a57d05246e594c63a777ea0c636284
Mate Lakat <mlakat@mlakat.hu>
2013-02-15 00:13:39 +0000

    Some cleanup, refactors

--- scripts/xs_start_create_vm_with_cdrom.sh
+++ scripts/xs_start_create_vm_with_cdrom.sh
@@ -28,23 +28,18 @@ TGT="$3"
 )
 }
 
-function create_archive
-{
-STAGING_DIR=$(mktemp -d)
-stage "$STAGING_DIR" "$1" customxs.iso
-stage "$STAGING_DIR" "$2" starter.sh
-(
-    cd "$STAGING_DIR"
-    tar -chzf - ./
-)
-rm -rf $STAGING_DIR
-}
-
 ISO_FILE="${1-$(print_usage_and_quit)}"
 XENSERVER="${2-$(print_usage_and_quit)}"
 NETWORK_NAME="${3-$(print_usage_and_quit)}"
 MACHINE_NAME="${4-$(print_usage_and_quit)}"
 
 THIS_DIR=$(dirname $(readlink -f $0))
-create_archive "$ISO_FILE" "$THIS_DIR/_xs_vm_starter.sh" |
+
+STAGING_DIR=$(mktemp -d)
+stage "$STAGING_DIR" "$ISO_FILE" customxs.iso
+stage "$STAGING_DIR" "$THIS_DIR/_xs_vm_starter.sh" starter.sh
+
+tar -C "$STAGING_DIR" -chzf - . |
 ssh "root@$XENSERVER" "mkdir vh && cd vh && tar -xzf - && bash starter.sh $NETWORK_NAME $MACHINE_NAME && cd .. && rm -rf vh"
+
+rm -rf "$STAGING_DIR"
